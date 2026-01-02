d9051f62ba6a5f1b23355c7779436eeca2cd042d
praveen-golagani <praveenkqa800@gmail.com>
2026-01-02 23:47:19 +0530

    cucumber bdd project - hooks & page fixture updated

--- src/step-definitions/Homepage_Steps.ts
+++ src/step-definitions/Homepage_Steps.ts
@@ -1,32 +1,31 @@
 import { Given, When } from "@cucumber/cucumber";
-import { Browser, chromium, Page } from "playwright";
-
-let browser: Browser; //browser instance
-let context: any; //browser context for sep browser session
-let page: Page; //webpage within context
+import { pageFixture } from "./hooks/browserContextFixture";
 
 const url = "https://webdriveruniversity.com";
 
 Given('I navigate to webdriveruniversity homepage', async () => {
-    //setup browser instance
-    browser = await chromium.launch({ headless: false });
-    context = await browser.newContext({ viewport: { width: 1440, height: 900 } });
-    page = await context.newPage();
     //access url
-    await page.goto(url);
+    await pageFixture.page.goto(url);
 });
 
 
 When('I click on the contact us button', async () => {
     //await page.pause();
-    const contactUs_Button = page.locator("//h1[text()='CONTACT US']");
+    const contactUs_Button = pageFixture.page.locator("//h1[text()='CONTACT US']");
     await contactUs_Button.click();
 
 });
 
 When('I switch to the new browser tab', async () => {
-    page = await context.waitForEvent("page"); // reinitialize the page as per new tab opened
-    await page.bringToFront();
+    pageFixture.page = await pageFixture.context.waitForEvent("page"); // reinitialize the page as per new tab opened
+    //retrive all open pages array
+    const allPages = pageFixture.context.pages();
+    //most recent
+    pageFixture.page = allPages[allPages.length - 1];
+    //move to newly opened tab
+    await pageFixture.page.bringToFront();
+    //maximize
+    await pageFixture.page.setViewportSize({ width: 1920, height: 1080 });
 });
 
 
--- src/step-definitions/hooks/hooks.ts
+++ src/step-definitions/hooks/hooks.ts
@@ -1,6 +1,10 @@
-import { AfterAll, BeforeAll, Before } from "@cucumber/cucumber";
+import { AfterAll, BeforeAll, Before,After } from "@cucumber/cucumber";
 import { Browser, chromium } from "playwright/test";
 import { pageFixture } from "./browserContextFixture";
+
+let browser: Browser;
+
+
 //Runs before All scenarios
 BeforeAll(async () => {
     console.log("\n Executing test suite..");
@@ -11,8 +15,15 @@ AfterAll(async () => {
     console.log("\n Finished Executing test suite..");
 });
 
+//before each scenario
 Before(async () => {
     browser = await chromium.launch({ headless: false });
-    context = await browser.newContext({ viewport: { width: 1440, height: 900 } });
-    page = await context.newPage();
+    pageFixture.context = await browser.newContext({ viewport: { width: 1920, height: 1080 } });
+    pageFixture.page = await pageFixture.context.newPage();
+});
+
+//Runs after each scenario
+After(async ()=>{
+    await pageFixture.page.close();
+    await browser.close();
 })
