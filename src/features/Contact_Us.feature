10ded644dac195a26e57c8bd45e3a7f5b0fe2e51
praveen-golagani <praveenkqa800@gmail.com>
2026-01-04 22:47:54 +0530

    handling dynamic test data

--- src/features/Contact_Us.feature
+++ src/features/Contact_Us.feature
@@ -1,5 +1,5 @@
 Feature: webdriveruniversity.com - Contact Us Page
-    Scenario: Valid Contact Us Form Submssion
+    Scenario: Valid Contact Us Form Submission
         Given I navigate to webdriveruniversity homepage
         When I click on the contact us button
         And I switch to the new browser tab
@@ -10,7 +10,7 @@ Feature: webdriveruniversity.com - Contact Us Page
         And I click on the submit button
         Then I should be presented with a successful contact us submission message
 
-    Scenario: Invalid Contact Us Form Submssion
+    Scenario: Invalid Contact Us Form Submission
         Given I navigate to webdriveruniversity homepage
         When I click on the contact us button
         And I switch to the new browser tab
@@ -20,3 +20,26 @@ Feature: webdriveruniversity.com - Contact Us Page
         And I type a comment
         And I click on the submit button
         Then I should be presented with a unsuccessful contact us submission message
+
+
+    Scenario: Valid Contact Us Form Submission Using Specific Data
+        Given I navigate to webdriveruniversity homepage
+        When I click on the contact us button
+        And I switch to the new browser tab
+        And I type a first name "Praveen test"
+        And I type a last name  "QA India"
+        And I enter an email address "test@testmail.com"
+        And I type a comment as "hello testers" use 147 rule to remember
+        And I click on the submit button
+        Then I should be presented with a successful contact us submission message
+
+    Scenario: Valid Contact Us Form Submission - Using Random Data
+        Given I navigate to webdriveruniversity homepage
+        When I click on the contact us button
+        And I switch to the new browser tab
+        And I type a random first name
+        And I type a random last name
+        And I enter a random email address
+        And I type a comment
+        And I click on the submit button
+        Then I should be presented with a successful contact us submission message
--- src/step-definitions/ContactUs_Steps.ts
+++ src/step-definitions/ContactUs_Steps.ts
@@ -1,6 +1,7 @@
 import { Then, When } from "@cucumber/cucumber";
 import { pageFixture } from "./hooks/browserContextFixture";
 import { test, expect } from '@playwright/test';
+import {faker} from '@faker-js/faker';
 
 
 
@@ -18,7 +19,7 @@ When('I enter an email address', async () => {
 
 
 When('I type a comment', async () => {
-    await pageFixture.page.locator("//textarea[@name='message']").fill(" Demo purpose text - Happy New Year !!");
+    await pageFixture.page.locator("//textarea[@name='message']").fill("Demo purpose text - Happy New Year !!");
 });
 
 When('I click on the submit button', async () => {
@@ -30,8 +31,48 @@ Then('I should be presented with a successful contact us submission message', as
     expect(text).toBe('Thank You for your Message!');
 });
 
-Then('I should be presented with a unsuccessful contact us submission message', async()=>{
+Then('I should be presented with a unsuccessful contact us submission message', async () => {
     const actInvalidText = await pageFixture.page.locator("body").textContent();
-    expect(actInvalidText).toMatch(/Error: (all fields are required|Invalid email address6)/ );
+    expect(actInvalidText).toMatch(/Error: (all fields are required|Invalid email address6)/);
 
+});
+
+//expressions
+
+When('I type a first name {string}', async (firstName: string) => {
+    await pageFixture.page.locator("//input[@name='first_name']").fill(firstName);
+    console.log(`Entered first name - ${firstName}`)
+});
+
+When('I type a last name  {string}', async (lastName: string) => {
+    await pageFixture.page.locator("//input[@name='last_name']").fill(lastName);
+    console.log(`Entered last name - ${lastName}`)
+});
+
+When('I enter an email address {string}', async (emailAddress: string) => {
+    await pageFixture.page.locator("//input[@name='email']").fill(emailAddress);
+    console.log(`Entered email - ${emailAddress}`)
+});
+
+When('I type a comment as {string} use {int} rule to remember', async (comText: string, comNum: number) => {
+    await pageFixture.page.locator("//textarea[@name='message']").fill(`Demo purpose text - ${comText} use ${comNum} rule to remember`);
+    console.log(`${comText} use ${comNum} rule to remember`);
+});
+
+//random data - faker
+
+When('I type a random first name', async () => {
+    const randomFirstName = faker.person.firstName();
+     await pageFixture.page.locator("//input[@name='first_name']").fill(randomFirstName);
+});
+
+When('I type a random last name', async () => {
+    const randomLastName = faker.person.lastName();
+    await pageFixture.page.locator("//input[@name='last_name']").fill(randomLastName);
+
+});
+
+When('I enter a random email address', async () => {
+    const randomEMail = faker.internet.email();
+    await pageFixture.page.locator("//input[@name='email']").fill(randomEMail);
 });
